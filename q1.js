5372d5b14ac085852bb7df6998cde1751cc752db
yj-yuanjiang <145314445+yj-yuanjiang@users.noreply.github.com>
2025-05-25 23:42:06 +1000

    q1.1 build an array
    Build an array with the given numbers and print it

---
+++ q1.js
@@ -0,0 +1,7 @@
+// Q1.1 Build an array with the given numbers and print it
+
+// Declare an array named 'numbers' and initialize it with the given numbers
+let numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+
+// Output the contents of the 'numbers' array to the browser console
+console.log(numbers);
